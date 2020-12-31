5877d1511ad5f84866e437fb6733dd2d70771495
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 13:27:26 +0000

    dictionary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-set1 = {"A", "B"}
-set2 = {"A", "C"}
-print(set1.intersection(set2))
-print(set1.difference(set2))
-print(set1.symmetric_difference(set2))
+samples = [
+  {"name": "Gagan", "age": 34},
+  {"name": "Gurleen", "age": 34},
+  {"name": "Bhavkirat", "age": 2.9}
+]
+print(samples[0]["name"])
+print(samples[1]["name"])
+print(samples[2]["name"])
