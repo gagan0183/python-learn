35b79ee300f91e9b42e672b82d739180d6615e17
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 19:28:06 +0000

    join values

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-list = [10, 20, 30, 40, 50, 10, 90, 99, 100, 1000]
-total = sum(list)
-print(total)
-
-length = len(list)
-print(length)
+values = ["A", "B", "C"]
+join_values = (", ").join(values)
+print(join_values)
