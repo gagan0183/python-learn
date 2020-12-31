1e7f1bef32fb6039a1ca86e7d578c9ee3f692ca3
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 13:32:17 +0000

    dictionary value

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 samples = [
-  {"name": "Gagan", "age": 34},
-  {"name": "Gurleen", "age": 34},
-  {"name": "Bhavkirat", "age": 2.9}
+  ("Gagan", 34),
+  ("Gurleen", 34),
+  ("Bhavkirat", 2.9)
 ]
-print(samples[0]["name"])
-print(samples[1]["name"])
-print(samples[2]["name"])
+print(dict(samples))
