2c8bc7031200b4965be8406cb7e25b6a472ab019
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 08:57:13 +0000

    convert input to int

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-name = "Gagandeep"
-your_name = input("Enter your name: ")
-print(f"Your name is {your_name}. My name is {name}")
+age = input("Enter your age: ")
+age_num = int(age)
+print(f"You have lived for {age_num * 12} months")
