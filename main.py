0aedb29ffdeb2151771d7bcd7dbd541a5ebebaf5
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 19:38:34 +0000

    if else statements

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-values = ["A", "B", "C"]
-join_values = (", ").join(values)
-print(join_values)
+country = "Canada"
+input_country = input("Enter your country:")
+
+if input_country == country:
+  print("This is canada")
+else:
+  print("other country")
