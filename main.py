0c4e5a8462f4084754ae8a51e4aa694c3e7ef8be
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 12:25:15 +0000

    not add or remove using list methods in tuple

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 short_tuple = "A", "B" # tuple without brackets
 a_bit_clearer_tuple = ("A", "B") # tuple with brackets
 tuple_within_list = [("A", "B")] # tuple in a list
+
+# cant add or remove to a tuple using append or remove methods
+short_tuple = short_tuple + ("C",) # add to a tuple
+print(short_tuple)
