39cba6dc177e611c1f86428c2731d1e1c14e27c2
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 19:26:45 +0000

    sum and len functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-samples = [
-  ("Gagan", 34),
-  ("Gurleen", 34),
-  ("Bhavkirat", 2.9)
-]
-print(dict(samples))
+list = [10, 20, 30, 40, 50, 10, 90, 99, 100, 1000]
+total = sum(list)
+print(total)
+
+length = len(list)
+print(length)
