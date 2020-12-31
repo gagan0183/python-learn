b210f6dde9bdc690cce6edb7f1eeb66f29576e14
Gagandeep singh <g.deepsingh1@gmail.com>
2020-12-31 13:00:06 +0000

    advance sets

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-char_set = {"A", "B"}
-char_set.add("C")
-print(char_set)
-
-char_set.remove("C")
-print(char_set)
+set1 = {"A", "B"}
+set2 = {"A", "C"}
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
