90fe2c15794ce6164b24ef7b5116bb9d45e354fa
Gagandeep singh <g.deepsingh1@gmail.com>
2021-01-02 13:45:58 +0000

    list slicing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for n in range(2, 10):
-  for x in range(2, n):
-    if n % x == 0:
-      print(f"{n} = {x} * {n // x}")
-      break;
-  else:
-    print(f"{n} is prime")
+list = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+print(list[1:2])
+print(list[1:])
+print(list[:10])
+print(list[-3:])
+print(list[:-1])
+print(list[-5:-4])
+print(list[:]) # returns new list
