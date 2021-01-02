02816d7e92ebcfb5c26aaf58a68c8021b58c6a9e
Gagandeep singh <g.deepsingh1@gmail.com>
2021-01-02 14:18:14 +0000

    comprehension set

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 list1 = ['A', 'P', 'D', 'E']
 list2 = ['A', 'P', 'B']
 
-list = [
+set = {
   value
   for value in list2
   if value.lower() in [val.lower() for val in list1]
-]
+}
 
-print(list)
+print(set)
