b6577a19b60fcfc8b30a4fb95a83656766984f8e
Gagandeep singh <g.deepsingh1@gmail.com>
2021-01-02 07:10:53 +0000

    dict iterate using items function

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-values = [("Gagan", 34), ("Gurleen", 35), ("Bhavkirat", 2.9)]
-for name, age in values:
+values = {"Gagan": 34, "Gurleen": 35, "Bhavkirat": 2.9}
+for name, age in values.items():
   print(f"{name} has age {age}")
